edf1943ece2a86ecd66b43b88619c9db462acbac
perlajarillo <perlai.jarillo@gmail.com>
2018-01-08 16:52:10 -0500

    Single card to remind somebody eating healthy.

---
+++ HealthyTip2/app/src/main/java/com/example/ptc2/healthytip2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ptc2.healthytip2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
